faf8e77799dfd3de4a50a64104433ddd09de5229
lizardqueenlexi <105025397+lizardqueenlexi@users.noreply.github.com>
2023-05-10 19:42:33 -0500

    Gives a better description for the antags menu.

--- tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/contractkiller.ts
+++ tgui/packages/tgui/interfaces/PreferencesMenu/antagonists/antagonists/contractkiller.ts
@@ -6,9 +6,15 @@ const ContractKiller: Antagonist = {
   name: 'Contract Killer',
   description: [
     multiline`
-    Plain and simple, you're here to kill someone. Gather intel, kill
-    your target, and get home to get your pay.
+      Plain and simple, you're here to kill someone. Gather intel, kill
+      your target, and get home to get your pay.
     `,
+
+    multiline`
+		  Contract Killers are a minor antagonist tasked to kill a single person.
+      Spend long enough near them first, and you can pick one of three random
+      kits to help. Avoid collateral damage.
+	  `,
   ],
   category: Category.Midround,
 };
